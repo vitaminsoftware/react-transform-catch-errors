96e49495f2e153c1f23da36351884d6619aba7c4
Vladimir Caruntu <vladimir.caruntu@gmail.com>
2016-07-20 18:21:39 +0300

    Report errors to sentry and continue rendering

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 export default function catchErrors({ filename, components, imports }) {
-  const [React, ErrorReporter, reporterOptions] = imports;
-
-  if (!React || !React.Component) {
-    throw new Error('imports[0] for react-transform-catch-errors does not look like React.');
-  }
-  if (typeof ErrorReporter !== 'function') {
-    throw new Error('imports[1] for react-transform-catch-errors does not look like a React component.');
-  }
-
   return function wrapToCatchErrors(ReactClass, componentId) {
     const originalRender = ReactClass.prototype.render;
 
@@ -18,25 +9,24 @@ export default function catchErrors({ filename, components, imports }) {
           return originalRender.apply(this, arguments);
         } catch (err) {
           setTimeout(() => {
-            if (typeof console.reportErrorsAsExceptions !== 'undefined') {
-              let prevReportErrorAsExceptions = console.reportErrorsAsExceptions;
-              // We're in React Native. Don't throw.
-              // Stop react-native from triggering its own error handler
-              console.reportErrorsAsExceptions = false;
-              // Log an error
-              console.error(err);
-              // Reactivate it so other errors are still handled
-              console.reportErrorsAsExceptions = prevReportErrorAsExceptions;
-            } else {
-              throw err;
+            if (!__SERVER__) {
+              if (window && window.Raven) {
+                window.Raven.captureException(err);
+              }
             }
+            // throw err;
           });
 
-          return React.createElement(ErrorReporter, {
-            error: err,
-            filename,
-            ...reporterOptions
-          });
+          return (
+            <span
+              style={{
+                background: 'red',
+                color: 'white'
+              }}
+            >
+              Render error!
+            </span>
+          );
         }
       }
     });
